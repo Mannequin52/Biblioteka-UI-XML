2f8a6dea57b0cb32d8bc664cc78f7146b3cdd329
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2024-02-05 09:33:51 +0100

    Add files via upload

---
+++ Biblioteka-AS/Biblioteka-AS/Korisnici.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Biblioteka_AS
+{
+    public partial class Korisnici : Form
+    {
+        List<KorisnikClass> korisnikList = new List<KorisnikClass>();
+        public Korisnici()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Korisnici_Load(object sender, EventArgs e)
+        {
+            ispisKorisnicitxt.Text = "Ime:" + "\nPrezime:" + "\nGod:" + "\nBroj tel.:" + "\nEmail:" + "\nOIB:" + "\nAdresa:";
+
+            foreach (KorisnikClass upiskorisnik in korisnikList)
+            {
+                ispisKorisnicitxt.AppendText("\n\n" + upiskorisnik.ToString());
+            }
+        }
+
+        private void osvjezibtn_Click(object sender, EventArgs e)
+        {
+            this.Update();
+        }
+
+        private void spremikorisnikbtn_Click(object sender, EventArgs e)
+        {
+            var dokumentXmlKorisnik = new XDocument();
+            var rootElem = new XElement("Korisnici");
+            dokumentXmlKorisnik.Add(rootElem);
+            foreach (KorisnikClass korisnik in korisnikList)
+            {
+
+                var korisnikElem = new XElement("Korisnik",
+                    new XAttribute("Ime", korisnik.ime),
+                    new XAttribute("Prezime", korisnik.prezime),
+                    new XAttribute("Godina-rod", korisnik.god),
+                    new XElement("Broj-tel", korisnik.brtel),
+                    new XElement("E-mail", korisnik.email),
+                    new XElement("OIB", korisnik.oib),
+                    new XElement("Adresa", korisnik.adresa));
+                rootElem.Add(korisnikElem);
+
+            }
+            foreach (KorisnikClass korisnik in korisnikList)
+            {
+                xmlispis.AppendText(dokumentXmlKorisnik.ToString());
+            }
+
+        }
+    }
+}
